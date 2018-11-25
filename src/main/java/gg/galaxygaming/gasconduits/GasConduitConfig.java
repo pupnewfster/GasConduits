c978c6c4816012b4b64988b3f0e3e9b03fccfc77
pupnewfster <richard@freimer.com>
2018-11-25 14:51:16 -0500

    Add commends to oonfig Adjust default config so that ender condutis are on par with ultimate tubes and advanced conduits are between advanced and elite tubes

--- src/main/java/gg/galaxygaming/gasconduits/GasConduitConfig.java
+++ src/main/java/gg/galaxygaming/gasconduits/GasConduitConfig.java
@@ -1,14 +1,39 @@
 package gg.galaxygaming.gasconduits;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
 
 @Config(modid = GasConduitsConstants.MOD_ID)
 public class GasConduitConfig {
-    //TODO: Add comments describing configs
+    //TODO: See if these are actually synced from server
+    @Comment({
+            "Millibuckets per tick extracted by a gas conduit's auto extracting. " +
+                    "(synced from server) [range: 1 ~ 1000000000, default: 64]"
+    })
     public static int tier1_extractRate = 64;
+    @Comment({
+            "Millibuckets per tick that can pass through a single connection to a gas conduit. " +
+                    "(synced from server) [range: 1 ~ 1000000000, default: 256]"
+    })
     public static int tier1_maxIO = 256;
-    public static int tier2_extractRate = 256;
-    public static int tier2_maxIO = 1024;
-    public static int tier3_extractRate = 1024;
-    public static int tier3_maxIO = 4096;
+    @Comment({
+            "Millibuckets per tick extracted by an advanced gas conduit's auto extracting. " +
+                    "(synced from server) [range: 1 ~ 1000000000, default: 512]"
+    })
+    public static int tier2_extractRate = 512;
+    @Comment({
+            "Millibuckets per tick that can pass through a single connection to an advanced gas conduit. " +
+                    "(synced from server) [range: 1 ~ 1000000000, default: 2048]"
+    })
+    public static int tier2_maxIO = 2048;
+    @Comment({
+            "Millibuckets per tick extracted by an ender gas conduit's auto extracting. " +
+                    "(synced from server) [range: 1 ~ 1000000000, default: 4096]"
+    })
+    public static int tier3_extractRate = 4096;
+    @Comment({
+            "Millibuckets per tick that can pass through a single connection to an ender gas conduit. " +
+                    "(synced from server) [range: 1 ~ 1000000000, default: 16384]"
+    })
+    public static int tier3_maxIO = 16384;
 }
