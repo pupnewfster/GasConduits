d9c7ce76d77967c2fef9d0be18ffb89aeb25cd36
pupnewfster <richard@freimer.com>
2018-11-26 15:26:08 -0500

    Fix arrows not rendering properly on gas conduits

--- src/main/java/gg/galaxygaming/gasconduits/GasConduitsConstants.java
+++ src/main/java/gg/galaxygaming/gasconduits/GasConduitsConstants.java
@@ -9,7 +9,8 @@ public class GasConduitsConstants {
     //Proxy Constants
     public static final String PROXY_COMMON = "gg.galaxygaming.gasconduits.common.CommonProxy";
     public static final String PROXY_CLIENT = "gg.galaxygaming.gasconduits.client.ClientProxy";
-    public static final String DEPENDENCIES = "required-after:enderioconduits@[5.0.38,);required-after:mekanism;";
+    public static final String DEPENDENCIES = "required-after:enderioconduits@[5.0.38,);required-after:mekanism;" +
+            "before:enderioconduitsappliedenergistics;before:enderioconduitsopencomputers;before:enderioconduitsrefinedstorage;";
 
     public static final int GAS_VOLUME = 1000;
     public static final int GAS_MAX_EXTRACTED_SCALER = 2;
