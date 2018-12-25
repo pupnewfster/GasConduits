252774a978ae8df101cabd139af434792114300e
pupnewfster <richard@freimer.com>
2018-12-25 09:32:45 -0500

    Compile against EnderIO Maven now that it is updated

--- src/main/java/gg/galaxygaming/gasconduits/GasConduitsConstants.java
+++ src/main/java/gg/galaxygaming/gasconduits/GasConduitsConstants.java
@@ -3,7 +3,7 @@ package gg.galaxygaming.gasconduits;
 public class GasConduitsConstants {
     public static final String MOD_ID = "gasconduits";
     public static final String MOD_NAME = "GasConduits";
-    public static final String VERSION = "1.12.2-1.1.0";
+    public static final String VERSION = "1.12.2-1.0.0";
     public static final String MC_VERSION = "1.12.2,";
 
     //Proxy Constants
